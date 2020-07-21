162f1cb664e295d0f9f466819f3dcd5f93e35945
Vitaly Turovsky <vital2580@icloud.com>
2020-07-22 00:48:04 +0300

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { CssBaseline, makeStyles, Button, Typography, ThemeProvider, createMuiTheme } from "@material-ui/core";
+import { GitHub as GithubIcon } from "@material-ui/icons";
+import { Twitch as TwitchIcon } from "@icons-pack/react-simple-icons";
+import { blue } from "@material-ui/core/colors";
+import { DiscussionEmbed } from "disqus-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props {
 }
+
+const useStyles = makeStyles({
+    root: {
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column"
+    },
+    mainContainer: {},
+    buttonsContainer: {
+        display: "flex",
+        justifyContent: "space-around",
+        marginTop: "10px"
+    },
+    center: {
+        textAlign: "center"
+    },
+    iconStartButton: {
+        marginRight: 5
+    },
+    siteComments: {
+        position: "absolute",
+        top: "calc(100vh - 80px)",
+        width: "800px"
+    }
+});
+
+const appTheme = createMuiTheme({
+    palette: {
+        type: "dark",
+        primary: blue,
+        secondary: {
+            main: "#9146FF"
+        }
+    }
+});
+
+let ArrowsRight: React.FC = () => (
+    <code style={{
+        fontSize: "1.1em",
+        margin: "0 5px"
+    }}>{">".repeat(2)}</code>
+);
+
+let App: React.FC<Props> = () => {
+    let classes = useStyles();
+
+    return <ThemeProvider theme={appTheme}>
+        <CssBaseline />
+        <div className={classes.root}>
+            <div className={classes.mainContainer}>
+                <Typography
+                    variant="h1"
+                    component="h1"
+                    className={classes.center}
+                >
+                    Ruining The Games
+                </Typography>
+                <div className={classes.buttonsContainer}>
+                    {/* BUTTONS */}
+                    <Button
+                        size="large"
+                        variant="outlined"
+                        startIcon={<GithubIcon />}
+                        color="primary"
+                    >
+                        Dead Github Acc
+                    </Button>
+                    <Button
+                        size="large"
+                        variant="outlined"
+                        startIcon={<TwitchIcon />}
+                        color="secondary"
+                    >
+                        Boring Twitch Channel
+                    </Button>
+                </div>
+            </div>
+            <div className={classes.siteComments}>
+                <DiscussionEmbed
+                    shortname="zardoy"
+                    config={{
+                        identifier: "HOME_PAGE"
+                    }}
+                />
+            </div>
+        </div>
+    </ThemeProvider>;
+};
 
 export default App;
