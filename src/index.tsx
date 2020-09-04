565ad0802867c30808851ba4a530586b40309ce3
Vitaly Turovsky <vital2580@icloud.com>
2020-09-05 01:41:06 +0300

    feat(dx): clear data on page refresh

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
+if (process.env.NODE_ENV === "development") {
+    console.clear();
+    localStorage.clear();
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
