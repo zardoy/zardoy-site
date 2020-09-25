a297382b6d11c10d4de0a41afeabd8bc38928331
Vitaly Turovsky <vital2580@icloud.com>
2020-09-25 21:52:53 +0300

    refactor with react-player + soundcloud player

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,102 +1,59 @@
-import React, { useRef, useEffect, useState, useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import { PlayArrow as PlayArrowIcon, Pause as PauseIcon } from "@material-ui/icons";
-import YoutubePlayer from "youtube-player";
 import { Button } from "@material-ui/core";
 // import SoundCloudPlayer from "react-player/soundcloud";
-// import YoutubePlayer from "react-player/youtube";
+import ReactPlayer from "react-player";
 
 const BACKGROUND_VIDEO_ID = "pITfVZcUSeI";
-const SHOW_UNMUTE_WARNING_KEY = "showUnmuteWarning";
 
 interface Props {
 }
 
 let Video: React.FC<Props> = () => {
-    const videoRef = useRef(null as HTMLDivElement | null);
-
     const [musicPlaying, setMusicPlaying] = useState(false);
 
-    useEffect(() => {
-        if (!videoRef.current) throw new TypeError("React. Ref isn't ready yet.");
-        let localPlayer = YoutubePlayer(videoRef.current, {
-            videoId: BACKGROUND_VIDEO_ID,
-            playerVars: {
-                // player should be muted
-                // autoplay: 1,
-                controls: 0,
-                enablejsapi: 1,
-                loop: 1,
-                rel: 0,
-                playsinline: 1,
-                disablekb: 1,
-                iv_load_policy: 3,
-                //@ts-ignore
-                showinfo: 0
-            }
-        });
-        localPlayer.on("ready", () => {
-            const maxTries = 20;
-            const muteAndCheck = () => {
-                localPlayer.mute();
-                return localPlayer.isMuted();
-            };
-            if (!muteAndCheck()) {
-                let tries = 0;
-                // trying to mute the video :/
-                let interval = setInterval(() => {
-                    if (++tries >= maxTries || muteAndCheck()) {
-                        clearInterval(interval);
-                        localPlayer.playVideo();
-                    }
-                }, 100);
-            } else {
-                localPlayer.playVideo();
-            }
-        });
-        // handle loop. api is rly old
-        localPlayer.on("stateChange", (event) => {
-            const YT_PLAYER_STATE_ENDED = 0;
-            if (event.data === YT_PLAYER_STATE_ENDED) {
-                localPlayer.playVideo();
-                setTimeout(() => {
-                    localPlayer.playVideo();
-                }, 150);
-            }
-        });
-        // setPlayer(localPlayer);
+    const handleToggleMusicButtonClick = useCallback(() => {
+        setMusicPlaying(state => !state);
     }, []);
 
     return <>
-        {/* {
-            playerMuted ?
-                <Button
-                    variant="contained"
-                    startIcon={<PlayArrowIcon />}
-                    onClick={clickUnmuteButtonHandle}
-                >
-                    PLAY SOME GACHI
-                    </Button> :
-                <Button
-                    variant="contained"
-                    startIcon={<PauseIcon />}
-                    onClick={clickMuteButtonHandle}
-                >
-                    PAUSE GACHI
-                    </Button>
-        } */}
-        <div ref={videoRef} style={{
-            zIndex: -1,
-            position: "fixed",
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            width: "100%",
-            height: "100%",
-            overflow: "hidden"
-        }}>
-            {/* JS PLACES YT IFRAME HERE */}
-        </div>
+        <Button
+            variant="contained"
+            startIcon={musicPlaying ? <PauseIcon /> : <PlayArrowIcon />}
+            onClick={handleToggleMusicButtonClick}
+        >
+            {musicPlaying ? "PAUSE" : "PLAY SOME"} GACHI
+        </Button>
+        <ReactPlayer
+            url={`youtube.com/watch?v=${BACKGROUND_VIDEO_ID}`}
+            muted
+            loop
+            playsinline
+            playing={true}
+            config={{
+                youtube: {
+                    playerVars: {
+                        showinfo: 0
+                    }
+                }
+            }}
+            width={"100%"}
+            height={"100%"}
+            style={{
+                zIndex: -1,
+                position: "fixed",
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                overflow: "hidden"
+            }}
+        />
+        <ReactPlayer
+            url="https://soundcloud.com/user-778575298/gachimuchi-ultimate-medley-2"
+            playing={musicPlaying}
+            height={0}
+        />
     </>;
 };
 
