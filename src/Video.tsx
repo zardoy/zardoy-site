c35c972d061e5d24e486e8ef019efed1ed9ae1a8
Vitaly Turovsky <vital2580@icloud.com>
2020-09-25 21:30:32 +0300

    removed old button behavior

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,8 +1,9 @@
 import React, { useRef, useEffect, useState, useCallback } from "react";
-import { VolumeUp as VolumeUpIcon, VolumeOff as VolumeOffIcon } from "@material-ui/icons";
+import { PlayArrow as PlayArrowIcon, Pause as PauseIcon } from "@material-ui/icons";
 import YoutubePlayer from "youtube-player";
-import { YouTubePlayer } from "youtube-player/dist/types";
-import { Button, Dialog, DialogTitle, DialogContent, DialogActions } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+// import SoundCloudPlayer from "react-player/soundcloud";
+// import YoutubePlayer from "react-player/youtube";
 
 const BACKGROUND_VIDEO_ID = "pITfVZcUSeI";
 const SHOW_UNMUTE_WARNING_KEY = "showUnmuteWarning";
@@ -13,31 +14,7 @@ interface Props {
 let Video: React.FC<Props> = () => {
     const videoRef = useRef(null as HTMLDivElement | null);
 
-    const [player, setPlayer] = useState(null as null | YouTubePlayer);
-    const [playerMuted, setPlayerMuted] = useState(true);
-    const [showWarning, setShowWarning] = useState(false);
-
-    const handleWarningClose = useCallback(() => setShowWarning(false), []);
-    const handleUnmuteConfirm = useCallback(() => {
-        if (!player) throw new TypeError("Player hasn't initialized.");
-        handleWarningClose();
-        localStorage.setItem(SHOW_UNMUTE_WARNING_KEY, "false");
-        player.unMute();
-        setPlayerMuted(false);
-    }, [player, handleWarningClose]);
-    const clickUnmuteButtonHandle = useCallback(() => {
-        const showUnmuteWarning = localStorage.getItem(SHOW_UNMUTE_WARNING_KEY) === null;
-        if (showUnmuteWarning) {
-            setShowWarning(true);
-        } else {
-            handleUnmuteConfirm();
-        }
-    }, [handleUnmuteConfirm]);
-    const clickMuteButtonHandle = useCallback(() => {
-        if (!player) throw new TypeError("Player hasn't initialized.");
-        player.mute();
-        setPlayerMuted(true);
-    }, [player]);
+    const [musicPlaying, setMusicPlaying] = useState(false);
 
     useEffect(() => {
         if (!videoRef.current) throw new TypeError("React. Ref isn't ready yet.");
@@ -86,41 +63,27 @@ let Video: React.FC<Props> = () => {
                 }, 150);
             }
         });
-        setPlayer(localPlayer);
+        // setPlayer(localPlayer);
     }, []);
 
     return <>
-        {
+        {/* {
             playerMuted ?
                 <Button
                     variant="contained"
-                    startIcon={<VolumeUpIcon />}
+                    startIcon={<PlayArrowIcon />}
                     onClick={clickUnmuteButtonHandle}
                 >
-                    UNMUTE
+                    PLAY SOME GACHI
                     </Button> :
                 <Button
                     variant="contained"
-                    startIcon={<VolumeOffIcon />}
+                    startIcon={<PauseIcon />}
                     onClick={clickMuteButtonHandle}
                 >
-                    MUTE
+                    PAUSE GACHI
                     </Button>
-        }
-
-        <Dialog
-            open={showWarning}
-            onClose={handleWarningClose}
-        >
-            <DialogTitle>READ BEFORE UNMUTE!!!</DialogTitle>
-            <DialogContent>
-                THE FOLLOWING CONTENT (SOUND) MAY CONTAIN ELEMENTS THAT ARE NOT SUITABLE FOR SOME AUDIENCES.
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={handleWarningClose}>Cancel</Button>
-                <Button onClick={handleUnmuteConfirm}>UNMUTE!!!</Button>
-            </DialogActions>
-        </Dialog>
+        } */}
         <div ref={videoRef} style={{
             zIndex: -1,
             position: "fixed",
@@ -132,7 +95,7 @@ let Video: React.FC<Props> = () => {
             height: "100%",
             overflow: "hidden"
         }}>
-            {/* YT IFRAME GOES HERE */}
+            {/* JS PLACES YT IFRAME HERE */}
         </div>
     </>;
 };
