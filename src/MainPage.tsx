13b7e9c69b8f6c71fc0ecb865bbc8b33a129f752
Vitaly Turovsky <vital2580@icloud.com>
2020-12-24 01:30:12 +0300

    feat: remove irrelevant content

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Button, Grid, makeStyles, Typography } from "@material-ui/core";
+import { Button, Grid, makeStyles } from "@material-ui/core";
 import { GitHub as GithubIcon } from "@material-ui/icons";
 
 interface ComponentProps {
@@ -26,36 +26,21 @@ let MainPage: React.FC<ComponentProps> = () => {
     return <>
         <Grid
             container
-            justify="space-between"
-            direction="column"
+            justify="center"
             alignContent="center"
             className={classes.root}
         >
             {/* todo grid items */}
-            <Grid item>
-                <Typography className={classes.header} variant="h1">Welcome!</Typography>
-                <Typography style={{ textAlign: "center" }} variant="h6">Huge content coming soon!</Typography>
-            </Grid>
             {/* move to css */}
-            <Grid item style={{ textAlign: "center" }}>
-                <Button
-                    size="large"
-                    color="primary"
-                    // todo better error if using as rendered child
-                    startIcon={<GithubIcon />}
-                    rel="noreferrer noopener"
-                    href="https://github.com/zardoy"
-                >ME ON GITHUB</Button>
-            </Grid>
+            <Button
+                size="large"
+                color="primary"
+                // todo better error if using as rendered child
+                startIcon={<GithubIcon />}
+                rel="noreferrer noopener"
+                href="https://github.com/zardoy"
+            >ME ON GITHUB</Button>
             {/* todo grid item with container */}
-            <Grid item>
-                <Grid container justify="flex-end" style={{ width: "100%" }}>
-                    <Grid item>
-                        {/* todo another font */}
-                        <Typography variant="button" className={classes.stayReformed}>Stay Reformed...</Typography>
-                    </Grid>
-                </Grid>
-            </Grid>
         </Grid>
         {/* todo return back the comments block */}
     </>;
